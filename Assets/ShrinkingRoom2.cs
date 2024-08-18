4a682cc9cda3c3a437a909b7a58284c52219fff7
Trying to code <97489676+DragonTailSB@users.noreply.github.com>
2024-08-18 12:21:49 -0700

    hallway fixed and shrinking room working

--- Assets/ShrinkingRoom2.cs
+++ Assets/ShrinkingRoom2.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class ShrinkingRoom2 : MonoBehaviour
 {
-    
+    [Header("Room Parts")]
     [SerializeField] private Transform[] Walls;
+    [SerializeField] private Transform[] Ceiling;
+    public GameObject[] props;
+    public Transform center;
 
+    [Header("Variables")]
+    public float speed = 1f;
+    [SerializeField] private float ceilingMult = 0.5f;
+    [SerializeField] private float playerMult = 0.25f;
+    [SerializeField] private float wallMult = 1f;
 
+    public void Shrink()
+    {
+        for (int i = 0; i < Walls.Length; i++)
+        {
+            Walls[i].position = Vector3.MoveTowards(Walls[i].position, center.position, Time.deltaTime * speed * wallMult);
+        }
+    }
+
+    private void Update()
+    {
+        Shrink();
+    }
 
 }
